cf5979cc13478116979d91655d46fe041df2fcac
Ednan Dias <ednan.741@gmail.com>
2023-04-12 22:43:56 -0300

    add changes

--- src/server.js
+++ src/server.js
@@ -1,31 +1,31 @@
-import http from 'node:http'
-import { json } from './middlewares/json.js'
-import { routes } from './routes.js'
-import { extractQueryParams } from './utils/extract-query-params.js'
+import http from 'node:http';
+import { json } from './middlewares/json.js';
+import { routes } from './routes.js';
+import { extractQueryParams } from './utils/extract-query-params.js';
 
 const server = http.createServer(async (req, res) => {
-  const { method, url } = req
+  const { method, url } = req;
 
-  req.url = decodeURIComponent(url)
+  req.url = decodeURIComponent(url);
 
-  await json(req, res)
+  await json(req, res);
 
-  const route = routes.find(route => {
-    return route.method === method && route.path.test(url)
-  })
+  const route = routes.find((route) => {
+    return route.method === method && route.path.test(url);
+  });
 
   if (route) {
-    const routeParams = req.url.match(route.path)
+    const routeParams = req.url.match(route.path);
 
-    const { query, ...params } = routeParams.groups
+    const { query, ...params } = routeParams.groups;
 
-    req.params = params
-    req.query = query ? extractQueryParams(query) : {}
+    req.params = params;
+    req.query = query ? extractQueryParams(query) : {};
 
-    return route.handler(req, res)
+    return route.handler(req, res);
   }
 
-  return res.writeHead(404).end('Not found')
-})
+  return res.writeHead(404).end('Not found');
+});
 
-server.listen(3333)
+server.listen(3333);
